73fd03fcd4d585522d2a106b331423b770d1719c
root <ridwaniliyas0@gmail.com>
2023-09-09 05:54:42 -0700

    my commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** main - function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{	
+			putchar(i + 'o');
+			putchar(', ');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
